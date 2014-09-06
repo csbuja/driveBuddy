d4ab657aa7e72145e33afc4f0e115df04541e790
CHRISTOPHER SPENCER BUJA <csbuja@0587362340.wireless.umich.net>
2014-09-06 00:59:34 -0400

    new appjs

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // Module dependencies.
 var express = require('express');
+var driverNeeds = require('./driverNeeds') 
+
+var yelp = require("yelp").createClient({
+  consumer_key: "T0VjCY0WkEUOuyC5U46qMw", 
+  consumer_secret: "LwzcaQMBcdE2cz-iv5M3KDxHwCk",
+  token: "QF86lSA004Z3R5mbKmXLGVFaUGfLSTET",
+  token_secret: "HedWTyztvJe_cuVgPL0IwqsHYjs"
+});
 
 var app = express.createServer();
 
@@ -9,19 +17,16 @@ app.configure( function() {
 
 // Routes
 app.get('/', function(req, res) {
-    
-     
-    
+    res.send('This isn\'t built in yet');
 });
-
-
-
 
 app.get('/yelp', function(req, res) {
 	var typeParam = req.body.type;
-	
+	var lat = req.body.type;
+	var lon = req.body.type;
 
-
+	//sends an http response back to the frontend
+	driverNeeds.getNeeds(res,type,lat,lon, yelp);
 });
 
 
