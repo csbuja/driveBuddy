262a72217f1f953ca05b3180059fdb4d8cdb50a9
CHRISTOPHER SPENCER BUJA <csbuja@0587362340.wireless.umich.net>
2014-09-05 23:38:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Module dependencies.
+var express = require('express');
+
+var app = express.createServer();
+
+// Configuration
+app.configure( function() {
+});
+
+// Routes
+app.get('/', function(req, res) {
+    
+     
+    
+});
+
+
+
+
+app.get('/food', function(req, res) {
+
+
+
+});
+
+app.get('/gas', function(req, res) {
+
+
+
+});
+app.listen(3000);
