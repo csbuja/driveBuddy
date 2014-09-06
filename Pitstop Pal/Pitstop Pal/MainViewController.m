13d34778c06627274622eea9c2207fd20188ac4d
Colin Lockareff <colin@getlight.co>
2014-09-06 01:07:40 -0400

    Created the PitstopPal xcode project

---
+++ Pitstop Pal/Pitstop Pal/MainViewController.m
@@ -0,0 +1,43 @@
+//
+//  MainViewController.m
+//  Pitstop Pal
+//
+//  Created by Colin Lockareff on 9/6/14.
+//  Copyright (c) 2014 MHACKS PV. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Flipside View
+
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showAlternate"]) {
+        [[segue destinationViewController] setDelegate:self];
+    }
+}
+
+@end
