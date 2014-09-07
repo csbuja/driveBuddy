c35d86605f6f1e7e24dabc3d11e918cd82e83bbe
CHRISTOPHER SPENCER BUJA <csbuja@587213062.guest.umich.net>
2014-09-07 03:24:10 -0400

    removed frontend

--- Pitstop Pal/Pods/Pods-Pitstop Pal-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pitstop_Pal_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_Pitstop_Pal_AFNetworking
-@end
--- Pitstop Pal/Pods/Pods-Pitstop Pal-Google-Maps-iOS-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pitstop_Pal_Google_Maps_iOS_SDK : NSObject
-@end
-@implementation PodsDummy_Pods_Pitstop_Pal_Google_Maps_iOS_SDK
-@end
