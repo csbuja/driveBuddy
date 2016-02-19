16fe22267a5062c9129775938abd0e42c8991828
Austin Urlaub <urlaubau17@gmail.com>
2016-02-19 00:51:40 -0500

    UI design of survey view with all components together

--- ReactNativeFrontend/views/RestaurantSurveyView.js
+++ ReactNativeFrontend/views/RestaurantSurveyView.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var React = require('react-native');
+
+var Button = require('react-native-button');
+var Dimensions = require('Dimensions');
 var SurveyRestaurantSearch = require('../Components/SurveyRestaurantSearch');
 var SurveySelectedRestaurantList = require('../Components/SurveySelectedRestaurantList');
 
 var {
+    Image,
     StyleSheet,
     Text,
     View,
 } = React;
+
+var { width } = Dimensions.get('window');
 
 var RestaurantSurveyView = React.createClass({
     getInitialState: function() {
@@ -19,21 +25,36 @@ var RestaurantSurveyView = React.createClass({
 
     render: function() {
         var selectedInfo = Object.keys(this.state.selected).map((k) => { return this.state.selected[k]; });
+        var isNextDisabled = selectedInfo.length < 10 ? true : false;
+        var nextButtonText = isNextDisabled ? "Select 10 Restaurants" : "Next";
 
         return (
             <View style={styles.mainView}>
-                <Text style={styles.title}>Search resturants you like</Text>
-                <Text style={styles.subtitle}>Select at least 10</Text>
-                <View>
-                    <SurveyRestaurantSearch
-                        onPress={this._onRestaurantSelect}
-                        needed={selectedInfo}
-                    />
+                <View style={styles.top}>
+                    <Text style={styles.title}>Search resturants you like</Text>
+                    <Text style={styles.subtitle}>Select at least 10</Text>
                 </View>
-                <SurveySelectedRestaurantList
-                    onRestaurantRemove={this._onRestaurantRemove}
-                    restaurantInfo={selectedInfo}
-                />
+                <View style={styles.bottom}>
+                    <Image
+                        style={styles.background}
+                        source={require('../Images/london.jpg')}>
+                        <SurveyRestaurantSearch
+                            onPress={this._onRestaurantSelect}
+                        />
+                        <SurveySelectedRestaurantList
+                            onRestaurantRemove={this._onRestaurantRemove}
+                            restaurantInfo={selectedInfo}
+                        />
+                    </Image>
+                    <Button
+                        containerStyle={styles.buttonContainer}
+                        disabled={isNextDisabled}
+                        style={styles.button}
+                        styleDisabled={styles.button}
+                        onPress={this._onNextPress}>
+                        {nextButtonText}
+                    </Button>
+                </View>
             </View>
         );
     },
@@ -47,24 +68,56 @@ var RestaurantSurveyView = React.createClass({
     _onRestaurantRemove: function(id) {
         console.log('yo');
     },
+
+    _onNextPress: function() {
+        // TODO (urlauba): send survey results to server and handle view change
+    },
 });
 
 var styles = StyleSheet.create({
-    mainView: {
-        backgroundColor: 'red',
+    background: {
         flex: 1,
+        resizeMode: 'cover',
+        width: width,
+    },
+    bottom: {
+        flex: 0.8,
+        flexDirection: 'column',
+    },
+    buttonContainer: {
+        backgroundColor: '#3399ff',
+        flex: 0.12,
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: "#FFFFFF",
+    },
+    mainView: {
+        backgroundColor: '#FFFFFF',
+        flex: 1,
+        flexDirection: 'column',
+    },
+    sec: {
+        backgroundColor: 'blue',
+        flex: 1,
+        flexDirection: 'column',
     },
     subtitle: {
-        color: "#FFFFFF",
+        color: "#333333",
         fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
     },
     title: {
-        color: "#FFFFFF",
+        color: "#333333",
         fontSize: 26,
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    top: {
+        flex: 0.2,
+        flexDirection: 'column',
+        justifyContent: 'center',
     },
 });
 
