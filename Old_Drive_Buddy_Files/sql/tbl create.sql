135a5391b06245f6438ca8f3319f6190d6f17bbc
jingsss <Jing@0587358743.wireless.umich.net>
2016-02-20 15:06:11 -0500

    sql

--- Old_Drive_Buddy_Files/sql/tbl create.sql
+++ Old_Drive_Buddy_Files/sql/tbl create.sql
@@ -1,25 +1,37 @@
-drop table if exists foodcollection;
-create table foodcollection(
+drop table if exists user;
+create table user(
 	userid int not null,
-	name varchar(255),
-	lon double,
-	lat double
+	primary key(userid)
 );
 
-drop table if exists gascollection;
-create table gascollection(
-	userid int not null,
+drop table if exists resturant;
+create table resturant(
+	resturant_id int not null,
 	name varchar(255),
-	lon double,
-	lat double
+	foodtype int not null,
+	cost int not null,
+	rate double not null
+	primary key(resturant_id)
+);
+
+drop table if exists user_res;
+create table user_res(
+	userid int not null,
+	resturant_id int not null,
+	time timestamp,
+	primary key (userid, resturant_id),
+	foreign key userid references user,
+	foreign key resturant_id references resturant
 );
 
 drop table if exists sensordata;
 create table sensordata(
 	userid int not null,
-	time long,
+	time timestamp,
 	lon double,
 	lat double,
-	status int
+	status int,
+	primary key (userid, timestamp),
+	foreign key userid references user
 );
 
