ec403198dd24e7b9469621a8d806c3a689e90a51
jingsss <Jing@0587383971.wireless.umich.net>
2016-04-11 13:16:55 -0400

    add

--- Backend/driverNeeds.js
+++ Backend/driverNeeds.js
@@ -242,6 +242,8 @@ module.exports = {
 
 	write_file: function(userid, restaurant_id,index){
 		var deferred = Q.defer();
+		userid = userid.toString();
+		restaurant_id = userid.toString();
 		db.query('select * from rate where userid = \'' + userid +'\' and restaurant_id = \'' + restaurant_id + '\'',
 		function(err, result){
 			if (err) {
