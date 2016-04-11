d84778262398bb99c5f5c4bd6985098bf74a80db
jingsss <Jing@sunjingdeMacBook-Air.local>
2016-04-10 23:06:00 -0400

    add header

--- Backend/Utilities/k-foldGenerator.py
+++ Backend/Utilities/k-foldGenerator.py
@@ -1,8 +1,6 @@
 import numpy
 import csv
 def generate_whole(filename1, filename2):
-	filename1 = '../sql/data.csv'
-	filename2 = '../sql/dataset.txt'
 	f = open(filename2,'w')
 	with open(filename1, 'rb') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',')
@@ -11,6 +9,7 @@ def generate_whole(filename1, filename2):
 		for row in spamreader:
 			if first == 0:
 				first = 1
+				f.write("Res,User,Rate,FoodType\n")
 			else:
 			#for i in range(1, len(row) - 1):
 				print >> f, '\n'.join(str for str in [str(i)+ ',' + str(a) + ',' + row[a] + ',\"' + row[len(row)-1] +'\"'
@@ -19,26 +18,31 @@ def generate_whole(filename1, filename2):
 	f.close()
 	csvfile.close()
 def k_fold(k, filename, test_name, train_name):
-	filename = '../sql/dataset.txt'
-	test_name = '../sql/test-'+str(k)+'-fold.txt'
-	train_name = '../sql/train-'+str(k)+'-fold.txt'
 	f = open(filename, 'r')
-	line_num = f.read().count("\n")
+	line_num = f.read().count("\n")-1
 	f.close()
 	index = numpy.random.choice(line_num, line_num/10, replace = False)
 	f_test = open(test_name, 'w')
 	f_train = open(train_name, 'w')
+	f_test.write("Res,User,Rate,FoodType\n")
+	f_train.write("Res,User,Rate,FoodType\n")
 	with open(filename, 'rb') as f:
-		i = 0
+		i = -1
 		for line in f:
-			if i in index:
-				f_test.write(line)
-			else:
-				f_train.write(line)
+			if i >= 0:
+				if i in index:
+					f_test.write(line)
+				else:
+					f_train.write(line)
 			i = i + 1
 	f.close()
 	f_test.close()
 	f_train.close()
 
-generate_whole('','')
-k_fold(10, "", "", "")
+filename1 = '../sql/data.csv'
+filename2 = '../Datasets/dataset.txt'
+k = 10
+test_name = '../Datasets/test-'+str(k)+'-fold.txt'
+train_name = '../Datasets/train-'+str(k)+'-fold.txt'
+generate_whole(filename1,filename2)
+k_fold(k, filename2, test_name, train_name)
