759354770053fef22a9d0ebba605e72420002497
Christopher Buja <csbuja@umich.edu>
2016-04-11 21:44:46 -0400

    deleted some bad routes

--- Backend/app.js
+++ Backend/app.js
@@ -31,8 +31,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//simple test
-
 
 
 function toMiles(km){
@@ -150,51 +148,6 @@ app.all('/api/test/', function(req,res){
 
 });
 
-//TO DELETE ****
-/*
-app.all('/api/fooddata/:restaurant/:userid', function(req, res){
-	db.query('SELECT restaurant_id from restaurant where restaurant_id = ?', req.params.restaurant, function(err, result) {
-		if (err){
-			throw err
-		}else{
-			if(result.length == 0){
-				yelp.business(req.params.restaurant,
-				function(err, data){
-					if (err){
-						res.send(JSON.stringify([]));
-					}else{
-						for(var i = 0; i < (data.categories).length; ++i){
-							data.categories[i] = (data.categories[i])[1];
-						}
-						var term = {
-							restaurant_id: data.id,
-							name: data.name,
-							rate: data.rating,
-							foodtype: (data.categories).toString()
-						};
-						db.query('INSERT INTO restaurant SET ?', term,function(err, result) {
-							if (err) throw err;
-						});
-						var post = {userid: req.params.userid, restaurant_id:req.params.restaurant};
-						db.query('INSERT INTO user_res SET ?', post, function(err, result) {
-							if (err) throw err;
-						});
-					}
-				});
-			}else{
-				var post = {userid: req.params.userid, restaurant_id:req.params.restaurant};
-				db.query('INSERT INTO user_res SET ?', post, function(err, result) {
-					if (err) throw err;
-				});
-			}
-
-
-		}
-	});
-	res.send('Data sent');
-
-});
-*/
 //insert sensor data into sensordata table
 app.all('/api/sensordata/:lat/:lon/:status/:userid', function(req,res) {
 	//for speed maybe
@@ -307,76 +260,6 @@ app.get('/api/gas/:currentPosition/:lastPosition',function (req, res) {
     );
 });
 
-
-//use http://www.worldweatheronline.com/api/
-//5 query per second
-//250 query per day
-/*
-app.get('/api/weather/:lat/:lon',function (req, res) {
-	var API_KEY = "e189ac7eb58172bc86f984c5eadd2";
-	var lon = req.params.lon;
-	var lat = req.params.lat;
-	uri = "http://api.worldweatheronline.com/free/v2/weather.ashx?key=" + API_KEY + "&q=" + lat + "," + lon + "&num_of_days=1&format=json&fx=no";
-	request({
-		method: 'GET',
-		uri: uri,
-	}
-	, function (err, response, body){
-		if(err){
-			res.send("error");
-		}
-		else{
-			res.send(body);
-
-		}
-	});
-
-});
-*/
-/*
-//the google maps api call is limited to 100,000 requests per day
-app.get('/googlemaps/:lat1/:lon1/:lat2/:lon2/:interval', function(req,res){
-	var lon1 = req.params.lon1;
-	var lat1 = req.params.lat1;
-	var lon2 = req.params.lon2;
-	var lat2 = req.params.lat2;
-	var mileInterval = req.params.interval;
-	var API_KEY = "AIzaSyDv_GaD1jG3T4iKWyxksqVnrFJ1f-Mphh8";
-
-	request({
-		method: 'GET',
-		uri: "https://maps.googleapis.com/maps/api/directions/json?origin= " +  lat1 + "," + lon1 + "&destination=" +  lat2 + "," + lon2 + "&key=" + API_KEY ,
-		json: true
-	}
-	, function (err, response, body){
-		if(err) console.log(body.error_message);
-		var distance = 0;
-
-		var latLons = polyline.decode(body.routes[0].overview_polyline.points);
-		_.each(latLons, function(v,i){
-			if (i!=(latLons.length -1)) {
-				distance += calcDistance(v[0],v[1],latLons[i+1][0],latLons[i+1][1]);
-			}
-		});
-
-		var numPlaces = (toMiles(distance)/ mileInterval );
-		var numPitstops = numPlaces -1;
-
-		var index = Math.floor(latLons.length/numPitstops);
-		var curr_index = index;
-		var pitstops = [];
-		while(curr_index < (latLons.length-1)){
-			pitstops.push(latLons[curr_index]);
-			curr_index += index;
-		}
-
-		res.send(JSON.stringify({ 'pitstops': pitstops, 'body' : body }) );
-
-  	  }
-	);
-});
-
-*/
 console.log('App running on port: ' + port);
 
 app.listen(port);
