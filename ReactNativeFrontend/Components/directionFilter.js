4efb7b4e117594307d886d7fd7846ce90e990444
Christopher Buja <csbuja@umich.edu>
2017-01-21 16:34:09 -0800

    just redid a ton of work to actually calculate direction and filter out businesses. Tested everything excepted the direction estimator.. .which uses a beta binomial function to weight stuff

--- ReactNativeFrontend/Components/directionFilter.js
+++ ReactNativeFrontend/Components/directionFilter.js
@@ -1,11 +1,12 @@
-var math = require("mathjs")
-
+var math = require("mathjs");
+var DirectionUtilities = require("./directionUtilities");
 
 //Direction Filter uses a direction estimator
 
 //direction is a vector in R^2 of <north-south,east-west>
 var DirectionFilter = function(directionEstimator)
 {
+    var du = directionUtilities();
     var o = {
         //business is vector in R^2 of <north-south,east-west>
         isAheadWithLatLons: function(bus_lat,bus_lon,current_pos_lat,current_pos_lon,direction){
@@ -13,34 +14,11 @@ var DirectionFilter = function(directionEstimator)
         },
         isAheadWithBusinessPositionVector : function(business,direction){
             //dot product is positive
-            if(!direction) direction = directionEstimator.estimateDirection();
+            if(!direction) direction = directionEstimator.estimateDirectionAndVelocity()[0];
             return math.dot(business, direction) > 0;
         },
         convertLatLonToPositionVector : function(bus_lat,bus_lon,current_pos_lat,current_pos_lon){
-            return math.subtract(this.LLtoECEF(bus_lat,bus_lon), this.LLtoECEF(current_pos_lat,current_pos_lon))
-        },
-        //returns in units of miles
-        LLtoECEF: function(lat, lon){
-            lat = this.degreesToRadians(lat);
-            lon = this.degreesToRadians(lon); // from http://stackoverflow.com/questions/10473852/convert-latitude-and-longitude-to-point-in-3d-space - CodingAway
-            var radius = (6378137.0);       // Radius of the Earth (in meters)
-            var f = 1.0/298.257223563  // Flattening factor WGS84 Model
-            var cosLat = math.cos(lat);
-            var sinLat = math.sin(lat);
-            var FF     = math.square(1.0-f);
-            var C      = 1/math.sqrt(math.square(cosLat) + FF * math.square(sinLat));
-            var S      = C * FF;
-            var x = (radius * C )*cosLat * math.cos(lon);
-            var y = (radius * C )*cosLat * math.sin(lon);
-            var z = (radius * S )*sinLat;
-
-            return [x, y, z].map(this.metersToMiles);
-        },
-        degreesToRadians: function(degrees){
-            return degrees * (math.pi/180);
-        },
-        metersToMiles: function(meters){
-            return  0.000621371192237*meters;
+            return math.subtract(du.LLtoECEF(bus_lat,bus_lon), du.LLtoECEF(current_pos_lat,current_pos_lon))
         }
     }
     return o;
---
+++ ReactNativeFrontend/Components/directionUtilities.js
@@ -0,0 +1,52 @@
+var math = require("mathjs");
+var jStat = require('jStat').jStat;
+
+var DirectionUtilities = function(){
+    var o = {
+     //returns in units of miles
+        LLtoECEF: function(lat, lon){
+            lat = this.degreesToRadians(lat);
+            lon = this.degreesToRadians(lon); // from http://stackoverflow.com/questions/10473852/convert-latitude-and-longitude-to-point-in-3d-space - CodingAway
+            var radius = (6378137.0);       // Radius of the Earth (in meters)
+            var f = 1.0/298.257223563  // Flattening factor WGS84 Model
+            var cosLat = math.cos(lat);
+            var sinLat = math.sin(lat);
+            var FF     = math.square(1.0-f);
+            var C      = 1/math.sqrt(math.square(cosLat) + FF * math.square(sinLat));
+            var S      = C * FF;
+            var x = (radius * C )*cosLat * math.cos(lon);
+            var y = (radius * C )*cosLat * math.sin(lon);
+            var z = (radius * S )*sinLat;
+
+            return [x, y, z].map(this.metersToMiles);
+        },
+        degreesToRadians: function(degrees){
+            return degrees * (math.pi/180);
+        },
+        metersToMiles: function(meters){
+            return  0.000621371192237*meters;
+        },
+        toUnitVector:function(v){ //in 2 norm
+            return v.map(function(z){return z/math.sqrt(math.dot(v,v)) })
+        },
+        BetaBinom : function(alpha,beta,n,k) {
+            var part_1 = math.combinations(n,k)
+            var part_2 = jStat.betaln(k+alpha,n-k+beta)
+            var part_3 = jStat.betaln(alpha,beta)
+            
+            var result = (math.log(part_1) + part_2)- part_3
+            
+            return math.exp(result)
+        }
+    }
+    return o;
+}
+
+module.exports = DirectionUtilities
+
+
+//TODO - Move this to the automated tests area
+/*
+var du = DirectionUtilities()
+console.log(du.BetaBinom(1,1,2,1))  //should  be 1/3
+*/
